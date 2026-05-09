294d87a696056bf91750cf3bcf1f6f2cac0d4876
Rahul <rahuldusa30@gmail.com>
2026-05-09 10:11:47 +0530

    first commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 
 app.use(cors());
 app.use(express.json());
